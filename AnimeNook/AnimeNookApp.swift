70a3e5f7aba9095f39633c07ff1b2aabd449321f
DAO <daoseng33@gmail.com>
2024-11-05 10:21:56 +0800

    Initial commit

---
+++ AnimeNook/AnimeNookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeNookApp.swift
+//  AnimeNook
+//
+//  Created by DAO on 2024/11/4.
+//
+
+import SwiftUI
+
+@main
+struct AnimeNookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeNookTests/AnimeNookTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnimeNookTests.swift
+//  AnimeNookTests
+//
+//  Created by DAO on 2024/11/4.
+//
+
+import Testing
+
+struct AnimeNookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
