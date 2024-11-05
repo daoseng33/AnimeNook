f9a72e2acc4920e2bab3da44a91d2a036afbdd05
DAO <daoseng33@gmail.com>
2024-11-05 10:39:22 +0800

    chore: add JikanAPIService framework

---
+++ JikanAPIService/JikanAPIService/JikanAPIService.h
@@ -0,0 +1,18 @@
+//
+//  JikanAPIService.h
+//  JikanAPIService
+//
+//  Created by DAO on 2024/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JikanAPIService.
+FOUNDATION_EXPORT double JikanAPIServiceVersionNumber;
+
+//! Project version string for JikanAPIService.
+FOUNDATION_EXPORT const unsigned char JikanAPIServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JikanAPIService/PublicHeader.h>
+
+
---
+++ JikanAPIService/JikanAPIServiceTests/JikanAPIServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  JikanAPIServiceTests.swift
+//  JikanAPIServiceTests
+//
+//  Created by DAO on 2024/11/5.
+//
+
+import Testing
+@testable import JikanAPIService
+
+struct JikanAPIServiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
